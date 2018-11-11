db8e62ddae84bd5d60f644e2ab7ec2f6cf4ea854
xente <dev@xente.co>
2018-11-11 11:14:42 +0300

    [Chore #161861436] Add test for username while loging in

--- tests/test_auth_api_login.py
+++ tests/test_auth_api_login.py
@@ -6,6 +6,7 @@ from api import app
 class TestApiEndPoints(unittest.TestCase):
     default_login_endpoint = 'api/v1/auth/login'
     default_email = 'ecmugenyi@gmail.com'
+    default_username = 'columbus'
     default_password = '1234'
 
     def setUp(self):
@@ -19,7 +20,7 @@ class TestApiEndPoints(unittest.TestCase):
         )
 
         response = self.tester.post(
-            self.default_signup_endpoint,
+            self.default_login_endpoint,
             content_type='application/json',
             data=json.dumps(user)
         )
@@ -34,7 +35,7 @@ class TestApiEndPoints(unittest.TestCase):
         )
 
         response = self.tester.post(
-            self.default_signup_endpoint,
+            self.default_login_endpoint,
             content_type='application/json',
             data=json.dumps(user)
         )
@@ -49,7 +50,7 @@ class TestApiEndPoints(unittest.TestCase):
         )
 
         response = self.tester.post(
-            self.default_signup_endpoint,
+            self.default_login_endpoint,
             content_type='application/json',
             data=json.dumps(user)
         )
@@ -64,7 +65,7 @@ class TestApiEndPoints(unittest.TestCase):
         )
 
         response = self.tester.post(
-            self.default_signup_endpoint,
+            self.default_login_endpoint,
             content_type='application/json',
             data=json.dumps(user)
         )
@@ -72,17 +73,62 @@ class TestApiEndPoints(unittest.TestCase):
         reply = json.loads(response.data.decode())
         self.assertEqual(reply["data"], "Sorry, email already registered with another user!")
 
-def test_that_password_is_empty(self):
-        user = dict(
-            email = self.email,
-            password = ''
-        )
+    def test_that_password_is_not_empty(self):
+            user = dict(
+                email = self.email,
+                password = ''
+            )
 
-        response = self.tester.post(
-            self.default_signup_endpoint,
-            content_type='application/json',
-            data=json.dumps(user)
-        )
+            response = self.tester.post(
+                self.default_login_endpoint,
+                content_type='application/json',
+                data=json.dumps(user)
+            )
 
-        reply = json.loads(response.data.decode())
-        self.assertEqual(reply["data"], "Sorry, password can not be empty!")
+            reply = json.loads(response.data.decode())
+            self.assertEqual(reply["data"], "Sorry, password can not be empty!")
+
+    def test_that_username_is_not_empty(self):
+            user = dict(
+                username = '',
+                password = self.default_password
+            )
+
+            response = self.tester.post(
+                self.default_login_endpoint,
+                content_type='application/json',
+                data=json.dumps(user)
+            )
+
+            reply = json.loads(response.data.decode())
+            self.assertEqual(reply["data"], "Sorry, username can not be empty!")
+
+    def test_that_username_is_not_space(self):
+            user = dict(
+                username = ' ',
+                password = self.default_password
+            )
+
+            response = self.tester.post(
+                self.default_login_endpoint,
+                content_type='application/json',
+                data=json.dumps(user)
+            )
+
+            reply = json.loads(response.data.decode())
+            self.assertEqual(reply["data"], "Sorry, username should not contain a space!")
+
+    def test_that_username_has_no_invalid_characters(self):
+            user = dict(
+                email = '*columbus',
+                password = self.default_password
+            )
+
+            response = self.tester.post(
+                self.default_login_endpoint,
+                content_type='application/json',
+                data=json.dumps(user)
+            )
+
+            reply = json.loads(response.data.decode())
+            self.assertEqual(reply["data"], "Sorry, username should contain valid characters!")
