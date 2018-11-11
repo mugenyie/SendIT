b8ee6e4d3b1af53c168da2fb64f07ccdb1a5f4f2
xente <dev@xente.co>
2018-11-11 13:43:46 +0300

    [Chore #161859783] Add test parcel order delivery endpoints

--- tests/base.py
+++ tests/base.py
@@ -4,6 +4,7 @@ from api import app
 
 class BaseTestCase(unittest.TestCase):
 
+    # Default User information
     default_login_endpoint = 'api/v1/auth/login'
     default_signup_endpoint = 'api/v1/auth/signup'
     default_email = 'ecmugenyi@gmail.com'
@@ -12,6 +13,16 @@ class BaseTestCase(unittest.TestCase):
     default_firstname = 'Emmanuel'
     default_lastname = 'Mugenyi'
     default_othernames = 'Columbus'
+
+    # Default Parcel Information
+    default_weight = 4.34
+    default_weightmetric = 'Kg'
+    default_senton = '2018-01-08 04:05:06 +3:00'
+    default_deliveredon = '2018-01-20 12:05:06 +3:00'
+    default_status = 'PLACED'                               # PLACED, TRANSITING, DELIVERED 
+    default_from = 'Ntinda Complex, Ntinda - Kisaasi road'
+    default_to = 'Fort-portal Town'
+    default_currentlocation = 'Ntinda Shopping Centre'
     
     def setUp(self):
         self.client = app.test_client()
