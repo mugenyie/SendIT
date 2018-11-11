b1e56493f9b48e46ca09d48e0f30f8454be593bb
xente <dev@xente.co>
2018-11-11 16:55:36 +0300

    [Finishes #161865577] Add Swagger API Documentaion

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,25 @@
 from flask import Flask
-# Blueprints
+from flask import Flask
+from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 from api.api import api
 
 app = Flask(__name__)
+CORS(app)
 
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/api/v1'  
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  
+    API_URL,
+    config={  
+        'app_name': "SendIT API Documentation"
+    }
+)
+
+# Register blueprints
 app.register_blueprint(api)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
