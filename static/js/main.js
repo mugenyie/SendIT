7010a4ce72dc9e59fffa7aea0c04dbcc06ccdcc1
xente <dev@xente.co>
2018-11-23 14:33:33 +0300

    [Feature #162088617] Add user log out option

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,8 @@ function postLoginData(){
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
+    document.getElementById("spinner").style.display = 'flex'
+
     fetch('https://sendit-api-columbus.herokuapp.com/api/v1/auth/login', {
         method: 'POST',
         headers : new Headers(),
@@ -20,33 +22,39 @@ function postLoginData(){
             setCookie('firstname', data.data[0].user.firstname, 30),
             setCookie('lastname', data.data[0].user.lastname, 30),
             setCookie('othernames', data.data[0].user.othernames, 30),
-            setCookie('isadmin', data.data[0].user.isadmin, 30),
+            setCookie('isadmin', data.data[0].user.isadmin, 30)
+            document.getElementById("spinner").style.display = 'none'
             window.location.href = "home.html"
         }
+        )
+    .catch(function(err){
+        console.log(err)
+        document.getElementById("spinner").style.display = 'none'
+        document.getElementById("login-notice").textContent="Wrong username or password !!"
+    });
+}
+
+function postRegisterData(event){
+    event.preventDefault();
+
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    fetch('https://sendit-api-columbus.herokuapp.com/api/v1/auth/login', {
+        method: 'POST',
+        headers : new Headers(),
+        body:JSON.stringify({
+            username:username, 
+            password:password
+        })
+    }).then((res) => res.json())
+    .then(
+        // setCookie('user', data.),
+        (data) =>  console.log(data)
         )
     .catch((err)=>console.log(err))
 }
 
-// function postRegisterData(event){
-//     event.preventDefault();
-
-//     let username = document.getElementById('username').value;
-//     let password = document.getElementById('password').value;
-
-//     fetch('https://sendit-api-columbus.herokuapp.com/api/v1/auth/login', {
-//         method: 'POST',
-//         headers : new Headers(),
-//         body:JSON.stringify({
-//             username:username, 
-//             password:password
-//         })
-//     }).then((res) => res.json())
-//     .then(
-//         // setCookie('user', data.),
-//         (data) =>  console.log(data)
-//         )
-//     .catch((err)=>console.log(err))
-// }
 
 function setCookie(cname,cvalue,exmins) {
     var d = new Date();
@@ -74,6 +82,28 @@ function getCookie(cname) {
 function checkToken() {
     var token=getCookie("token");
     if (token == "") {
+        alert("Login to continue")
         window.location.replace('index.html');
     }
+}
+
+function logOut()
+{   
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++)
+    {   
+        var spcook =  cookies[i].split("=");
+        deleteCookie(spcook[0]);
+    }
+    function deleteCookie(cookiename)
+    {
+        var d = new Date();
+        d.setDate(d.getDate() - 1);
+        var expires = ";expires="+d;
+        var name=cookiename;
+        //alert(name);
+        var value="";
+        document.cookie = name + "=" + value + expires + "; path=/acc/html";                    
+    }
+    window.location = "index.html"; 
 }
--- static/js/parcel.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("username").textContent=getCookie('username');
-
-
-
-
-
